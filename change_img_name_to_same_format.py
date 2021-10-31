240fa0c12e434f47a5f6376a9ab650e735140a77
anderson0425 <93434928+anderson0425@users.noreply.github.com>
2021-10-31 23:31:40 +0800

    update code

--- change_img_name_to_same_format.py
+++ change_img_name_to_same_format.py
@@ -3,12 +3,14 @@ import os
 import cv2
 
 #透過這個path找到一系列的檔案及資料夾名稱
-parent_dir = "C:\Users\user\Downloads\img"
-files_and_directory_names = os.listdir(parent_dir)
+#parent_dir = "C:\Users\user\Downloads\img"
+#files_and_directory_names = os.listdir(parent_dir)
+files_and_directory_names = os.listdir()
 
 #根據有無substring ".jpg"和".png"判斷是否放入新生成的list
 #讓新list只有"圖片檔"的名稱
-photo_name = [ele   for ele in files_and_directory_names   if ".jpg" or ".png" in ele] 
+photo_name = [ele   for ele in files_and_directory_names   if ".jpg" in ele or ".png" in ele] 
+#print(photo_name)
 
 #讀取指定名稱的圖片，並改成imagexx.jpg的名稱
 i=0
@@ -16,5 +18,6 @@ index=list(range(8,100))  #改這裡
 for ele in photo_name:
     img = cv2.imread(ele)
     out_image_name = "image" + str(index[i]) + ele[-4:] 
+    print(out_image_name)
     cv2.imwrite(out_image_name, img)
     i+=1
